3446c48df2402badba67d620576f932f5502e218
haikal <haikal.putra1210@gmail.com>
2025-06-07 10:33:57 +0700

    memperbaiki berkas home-page dan menambahkan beberapa styling

--- src/scripts/pages/home/home-page.js
+++ src/scripts/pages/home/home-page.js
@@ -3,15 +3,13 @@ export default class HomePage {
   async render() {
     return `
       <section class="first-section">
-        <div class="fisrt-content>
+        <div class="fisrt-content">
+          <img class="ilustration" src="images/ilustrative-save-earth-outdoor.jpg" alt="Ilustrasi orang mengumpulkan sampah">
           <h1>Kenali Sampahmu, <span class="first-highlight">Mulai dari Sekarang!</span></h1>
           <p>Unggah foto sampah dan temukan jenis serta cara penanganan terbaiknya.<br>Pilah jadi mudah dengan bantuan teknologi!</p>
           <div class="toggle-button">
             <button class="toggle-btn">Mulai Kenali Sampahmu<i class="fa-solid fa-search"></i></button>
           </div>
-        </div>
-        <div class="ilustration>
-          <img src="ilustrative-save-earth-outdoor.jpg" alt="Ilustrasi orang mengumpulkan sampah">
         </div>
       </section>
 
@@ -23,17 +21,17 @@ export default class HomePage {
         </div>
         <div>
           <div class="card-container">
-            <img src="pile-of-rubbish.jpg"" alt="gambar tumpukan sampah">
+            <img src="images/pile-of-rubbish.jpg"" alt="gambar tumpukan sampah">
             <h3>Masalah Sampah yang <span class="third-highlight">Terabaikan</span></h3>
             <p>Kurangnya kebiasaan memilah sampah menyebabkan limbah menumpuk di TPA dan mencemari lingkungan sekitar kita.</p>
           </div>
           <div>
-            <img src="PilahCerdas-tecnology.jpg" alt="tekonologi pilah cerdas">
+            <img src="images/PilahCerdas-tecnology.jpg" alt="tekonologi pilah cerdas">
             <h3>Teknologi untuk Bantu<span class="third-highlight">Pilah Sampah</span></h3>
             <p>Dengan bantuan AI berbasis gambar, <span class="fourth-highlight">PilahCerdas</span> memudahkan siapa pun mengenali jenis sampah dan cara membuangnya.</p>
           </div>
           <div>
-            <img src="three-trash-cans" alt="gambar tiga tempat sampah">
+            <img src="images/three-trash-cans.jpg" alt="gambar tiga tempat sampah">
             <h3>Kontribusi untuk <span class="third-highlight">Bumi</span></h3>
             <p>setiap gambar yang kamu unggah membantu menciptakan kebiasaan baik, mengurangi penccemaran, dan membangun budaya pilah sampah di indonesia.</p>
           </div>
